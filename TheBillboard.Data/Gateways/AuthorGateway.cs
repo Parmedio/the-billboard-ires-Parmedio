5a3c9134b6c64c6058531ba67c2ddec425a905e8
Parmedio <marco.da.pieve@gmail.com>
2023-05-25 14:35:07 +0200

    link Author gateway to DB

--- TheBillboard.Data/Gateways/AuthorGateway.cs
+++ TheBillboard.Data/Gateways/AuthorGateway.cs
@@ -1,19 +1,17 @@
 namespace TheBillboard.Data.Gateways;
 
 using Abstract;
+using Microsoft.EntityFrameworkCore;
 using Models;
+using TheBillboard.Data.Data;
 
 public class AuthorGateway : IGateway<Author>
 {
-    private readonly IEnumerable<Author> _authors = new List<Author>
-    {
-        new(1, "Alberto", "Viezzi"),
-        new(2, "Dario", "Viezzi")
-    };
+    private readonly TheBillboardContext _context;
+    public AuthorGateway(TheBillboardContext context) => _context = context;
 
-    public IEnumerable<Author> GetAll() => _authors;
-
-    public Author? GetById(int id) => _authors.SingleOrDefault(author => author.Id == id);
+    public IEnumerable<Author> GetAll() => _context.Authors;
+    public Author? GetById(int id) => _context.Authors.SingleOrDefault(author => author.Id == id);
     public Author Insert(Author entity) => throw new NotImplementedException();
     public Author Modify(Author entity) => throw new NotImplementedException();
     public void Delete(int id) => throw new NotImplementedException();
